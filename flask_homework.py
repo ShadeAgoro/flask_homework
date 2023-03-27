7800a5b5850718cfb3b59be185272bd7faec3a00
Shade Agoro <Shadeagoro@hotmail.co.uk>
2023-03-27 20:53:03 +0100

    webpage with image that appears when clicking welcome hyperlink

--- flask_homework.py
+++ flask_homework.py
@@ -17,10 +17,9 @@ def say_hello_page(name):
 """.format(name)
 # triple quote marks displays data over multiple lines
 
-
 @app.route('/about/page')
 def about_page():
-     return Response ("This is the about page", mimetype='text/plain')
+     return Response("This is the about page", mimetype='text/plain')
 
 @app.route('/get/text')
 def get_text():
@@ -28,26 +27,32 @@ def get_text():
 @app.route("/index/<name>/<int:age>")
 def index(name, age):
     url = url_for('get_text')
+    image = url_for('static', filename='flask.jpg')
     return """
+
 <html>
 <head>
-    <title style="color:coral ; ">Sample - Flask routes</title>
+    <title style="color:coral ;">Sample - Flask routes</title>
 </head>
 
 <body>
-    <h1 style="color:coral ;">Welcome to our Website</h1>
+    <h1 style="color:aqua ;">Welcome to our Website</h1>
     <p>Hello {}!</p>
     <p>You are {} year(s) old.</p>
     <hr>
-    <h2 style="color: coral;"> Why Use Flask</h2>
-    <p>Flask is a web application framework written in Python.It was developed by Armin Ronacher, who led a team of 
-    international Python enthusiasts called Pooco. Flask is based on the Werkzeg WSGI toolkit and the Jinja2 template 
-    engine. Both are Pocco projects. </p> 
+    <h2 style="color: aqua;"> Why Use Flask</h2>
+    <img src="{}" height='300' width='300' alt="Image of Python Flask">
+    <p>A flask is a small container that holds a liquid. Most flasks have a squarish body and a small neck for 
+    drinking or pouring. Don't try sneaking a flask into your tube sock.There are several different kinds of flasks, 
+    including a "hip flask," small enough to fit in a pocket, for carrying and surreptitiously sipping alcoholic 
+    beverages. Chemists use another type of flask in the laboratory, which has a wide bottom and a small mouth and is 
+    made of glass. And in Britain, a flask is also a vacuum-sealed container that keeps drinks hot, usually called a 
+    "Thermos" in the US. </p> 
     <br>
     <a href= "{}">Welcome</a>
 </body>
 </html>
-""".format(name, age, url)
+""".format(name, age, url, image)
 
 
 if __name__ == "__main__":
